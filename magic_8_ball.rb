6971b9f6f8cfe038dbc5a9d4075aa7dfd600a11d
Kris Gray <kris.m.gray@gmail.com>
2017-08-17 16:49:10 -0600

    finished magic 8 ball

--- magic_8_ball.rb
+++ magic_8_ball.rb
@@ -1,10 +1,26 @@
 require 'pry'
 require 'colorize'
 
-@answers = [ 'No', 'Ask again', 'May the Force be with you',
-'Yes', 'I dont feel like helping you right now',
-'Why do you wish to know?', 'Back off Jack Weed!'
+@answers = [ 'No', 'Ask again', 'Yes',
+'I dont feel like helping you right now',
+'Why do you wish to know?', 'Back off Jack Weed!', 'Try Again'
  ]
+
+ @answers_default = @answers.clone
+
+def unique
+  @answers << gets.strip
+  @answers.length == @answers.uniq.length
+end
+
+def new_array
+  if unique == true
+    puts "Your answer has been added"
+  elsif unique == false
+    puts "That answer already exists"
+  end
+end
+
 
 def menu
   puts "Welcome to the magic 8 ball!".colorize(:orange)
@@ -16,7 +32,7 @@ def menu
     when '1'
       choice
     when 'QUIT'
-      puts "Beware, for knowlege is power."colorize(:red)
+      puts "Beware, for knowlege is power.".colorize(:red)
     else
       puts "Invalid entry, try again"
       menu
@@ -26,10 +42,8 @@ end
 def choice
     puts "What would you desire to know?".colorize(:green)
     puts "Type help for suggestions".colorize(:green)
-    puts "Type QUIT to leave at any time".colorize(:yellow)
+    puts "Press 3 to leave at any time".colorize(:yellow)
     input = gets.chomp
-    puts @answers.sample
-    choice
       case input
         when "What should I ask my Iphone?"
           puts 'Ask siri "I see a little silhoutto of a man"'
@@ -41,7 +55,26 @@ def choice
         puts "Ask this: What should I ask my phone?"
         puts "Or try this: Who is my father?"
         choice
-        when "QUIT"
+        when "add"
+          new_array
+          choice
+        when "list"
+           @answers.each_with_index do |response, index|
+          puts "#{index + 1}. #{response}"
+            end
+          puts ""
+          puts "If you want to add an answer the try typing add"
+          puts "Try typing reset if you want the original answers"
+          choice
+        when "reset"
+          puts "The original answers have returned"
+          @answers = @answers_default
+          choice
+        when '3'
+          puts "May the force be with you!".colorize(:blue)
+        else
+          puts @answers.sample
+          choice
       end
 end
 menu
